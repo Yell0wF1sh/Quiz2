e0189aeeddcc00e2a70c2a19c7f15c2604e5fb17
Yell0wF1sh <86252839+Yell0wF1sh@users.noreply.github.com>
2021-10-07 16:02:51 -0400

    16:02 10/7/21

--- MathQuiz/App.js
+++ MathQuiz/App.js
@@ -1,16 +1,55 @@
 
 import React, {useState} from 'react';
-import Random from 'expo-random'
-import { StyleSheet, Text, View } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
-  const [x, setX] = useState[0]
-  const [y, setY] = useState[0]
+  const [x, setX] = useState(Math.floor(Math.random()*13))
+  const [y, setY] = useState(Math.floor(Math.random()*13))
+  const [userAnswer, setUserAnswer] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const [answered, setAnswered] = useState(0)
+  const [isCorrect, setIsCorrect] = useState(null)
 
   const storeData = async (value) => {
 
   }
+
+  const checkAnswerAreaDefault =
+  (
+    <View style={{justifyContent: 'center', alignItems: 'flex-start'}}>
+      <Button
+        color='red'
+        title='CHECK ANSWER'
+      />
+    </View>
+  );
+
+  const checkAnswerAreaCorrect = 
+  (
+    <View style={{justifyContent: 'center', alignItems: 'flex-start'}}>
+      <Text style={{color:'red', fontWeight: 600}}>
+        CORRECT!!
+      </Text>
+      <Button
+        color='green'
+        title='Next Question'
+      />
+    </View>
+  );
+
+const checkAnswerAreaWrong = 
+  (
+    <View style={{justifyContent: 'center', alignItems: 'flex-start'}}>
+      <Text style={{color:'red', fontWeight: 600}}>
+        Sorry, answer was {x*y}, try again!
+      </Text>
+      <Button
+        color='green'
+        title='Next Question'
+      />
+    </View>
+  );
 
   return (
     <View>
@@ -20,7 +59,15 @@ export default function App() {
       <Text>
         Calculate the product of the following two numbers:
       </Text>
-      {setX(Random.getRandomBytes(100))}
+      <View style={{flexDirection: 'row'}}>
+        <Text>
+          {x} * {y} = 
+        </Text>
+        <TextInput 
+          placeholder='???'
+        />
+      </View>
+      {isCorrect?checkAnswerAreaCorrect:checkAnswerAreaDefault}
     </View>
   );
 }
