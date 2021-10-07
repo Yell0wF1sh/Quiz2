874e6488c00de4c0a4c9be4af4a4988b69d03ebe
Yell0wF1sh <86252839+Yell0wF1sh@users.noreply.github.com>
2021-10-07 11:57:28 -0400

    11:57 10/7/21

--- MathQuiz/App.js
+++ MathQuiz/App.js
@@ -1,5 +1,6 @@
 
 import React, {useState} from 'react';
+import Random from 'expo-random'
 import { StyleSheet, Text, View } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -8,18 +9,18 @@ export default function App() {
   const [y, setY] = useState[0]
 
   const storeData = async (value) => {
-    
+
   }
 
   return (
     <View>
-      <Text>
+      <Text style={{fontSize: 32, color: 'blue', fontWeight: 600}}>
         Math quiz for numbers between 0 and 12
       </Text>
       <Text>
         Calculate the product of the following two numbers:
       </Text>
-
+      {setX(Random.getRandomBytes(100))}
     </View>
   );
 }
