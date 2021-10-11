2159fe736351702250fba2cf238f392b9bba3048
Yell0wF1sh <86252839+Yell0wF1sh@users.noreply.github.com>
2021-10-11 13:32:39 -0400

    13:32 10/11/21

--- MathQuiz/App.js
+++ MathQuiz/App.js
@@ -1,73 +1,168 @@
 
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 // import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
-  const [x, setX] = useState(Math.floor(Math.random()*13))
-  const [y, setY] = useState(Math.floor(Math.random()*13))
+  // const [x, setX] = useState(Math.floor(Math.random() * 13))
+  const [x, setX] = useState(5)
+  // const [y, setY] = useState(Math.floor(Math.random() * 13))
+  const [y, setY] = useState(10)
   const [userAnswer, setUserAnswer] = useState(0)
+  // const [correctAnswer, setCorrectAnswer] = useState(x * y)
+  const [correctAnswer, setCorrectAnswer] = useState(50)
   const [correct, setCorrect] = useState(0)
   const [answered, setAnswered] = useState(0)
-  const [isCorrect, setIsCorrect] = useState(null)
+  const [isCorrect, setIsCorrect] = useState(false)
+  const [isChecking, setIsChecking] = useState(false)
+  // const [isCorrect, setIsCorrect] = useState(null)
 
   const storeData = async (value) => {
 
   }
 
-  const checkAnswerAreaDefault =
-  (
-    <View style={{justifyContent: 'center', alignItems: 'flex-start'}}>
-      <Button
-        color='red'
-        title='CHECK ANSWER'
-      />
-    </View>
-  );
+  // const CheckAnswerArea = () => {
+  //   if (userAnswer == correctAnswer) {
+  //     return (
+  //       <View style={{ justifyContent: 'center', alignItems: 'flex-start' }}>
+  //         <Text style={{ color: 'red', fontWeight: 600 }}>
+  //           CORRECT!!
+  //         </Text>
+  //         <Button
+  //           color='green'
+  //           title='Next Question'
+  //           onPress={(userAnswer == correctAnswer) ? setIsCorrect(true) : setIsCorrect(false)}
+  //         />
+  //       </View>
+  //     )
+  //   }
+  //   else if (isCorrect == false) {
+  //     return (
+  //       <View style={{ justifyContent: 'center', alignItems: 'flex-start' }}>
+  //         <Text style={{ color: 'red', fontWeight: 600 }}>
+  //           Sorry, answer was {x * y}, try again!
+  //         </Text>
+  //         <Button
+  //           color='green'
+  //           title='Next Question'
+  //           onPress={(userAnswer == correctAnswer) ? setIsCorrect(true) : setIsCorrect(false)}
+  //         />
+  //       </View>
+  //     )
+  //   }
+  //   else if (isCorrect == null) {
+  //     return (
+  //       <View style={{ justifyContent: 'center', alignItems: 'flex-start' }}>
+  //         <Button
+  //           color='red'
+  //           title='CHECK ANSWER'
+  //           onPress={(userAnswer == correctAnswer) ? setIsCorrect(true) : setIsCorrect(false)}
+  //         />
+  //       </View>
+  //     )
+  //   }
+  // }
 
-  const checkAnswerAreaCorrect = 
-  (
-    <View style={{justifyContent: 'center', alignItems: 'flex-start'}}>
-      <Text style={{color:'red', fontWeight: 600}}>
-        CORRECT!!
-      </Text>
-      <Button
-        color='green'
-        title='Next Question'
-      />
-    </View>
-  );
+  // const CheckAnswerArea = () => {
+  //   if (userAnswer == correctAnswer) {
+  //     return (
+  //       <View style={{ justifyContent: 'center', alignItems: 'flex-start' }}>
+  //         <Text style={{ color: 'red', fontWeight: 600 }}>
+  //           CORRECT!!
+  //         </Text>
+  //       </View>
+  //     )
+  //   } else {
+  //     return (
+  //       <View style={{ justifyContent: 'center', alignItems: 'flex-start' }}>
+  //         <Text style={{ color: 'red', fontWeight: 600 }}>
+  //           Sorry, answer was {x * y}, try again!
+  //         </Text>
+  //       </View>
+  //     )
+  //   }
 
-const checkAnswerAreaWrong = 
-  (
-    <View style={{justifyContent: 'center', alignItems: 'flex-start'}}>
-      <Text style={{color:'red', fontWeight: 600}}>
-        Sorry, answer was {x*y}, try again!
-      </Text>
-      <Button
-        color='green'
-        title='Next Question'
-      />
-    </View>
-  );
+  // }
+
+  // const CheckAnswer = () => {
+  //   return (
+  //     <View style={{ justifyContent: 'center', alignItems: 'flex-start' }}>
+  //       <Button
+  //         color='red'
+  //         title='CHECK ANSWER'
+  //         onPress={setIsChecking(true)}
+  //       />
+  //     </View>
+  //   )
+  // }
+
+  // const NextQuestion = () => {
+  //   if (userAnswer == correctAnswer) {
+  //     return (
+  //       <View style={{ justifyContent: 'center', alignItems: 'flex-start' }}>
+  //         <Text style={{ color: 'red', fontWeight: 600 }}>
+  //           CORRECT!!
+  //         </Text>
+  //         <Button
+  //           color='green'
+  //           title='Next Question'
+  //           onPress={setIsChecking(false)}
+  //         />
+  //       </View>
+  //     )
+  //   } else {
+  //     return (
+  //       <View style={{ justifyContent: 'center', alignItems: 'flex-start' }}>
+  //         <Text style={{ color: 'red', fontWeight: 600 }}>
+  //           Sorry, answer was {correctAnswer}, try again!
+  //         </Text>
+  //         <Button
+  //           color='green'
+  //           title='Next Question'
+  //           onPress={setIsChecking(false)}
+  //         />
+  //       </View>
+  //     )
+  //   }
+  // }
 
   return (
     <View>
-      <Text style={{fontSize: 32, color: 'blue', fontWeight: 600}}>
+      <Text style={{ fontSize: 32, color: 'blue', fontWeight: 600 }}>
         Math quiz for numbers between 0 and 12
       </Text>
       <Text>
         Calculate the product of the following two numbers:
       </Text>
-      <View style={{flexDirection: 'row'}}>
+      <View style={{ flexDirection: 'row' }}>
         <Text>
-          {x} * {y} = 
+          {x} * {y} =
         </Text>
-        <TextInput 
+        <TextInput
           placeholder='???'
+          onChangeText={text => setUserAnswer(text)}
         />
       </View>
-      {isCorrect?checkAnswerAreaCorrect:checkAnswerAreaDefault}
+      {/* {isCorrect
+        ?
+        <Text style={{ color: 'red', fontWeight: 600 }}>
+          CORRECT!!
+        </Text>
+        : */}
+      <Text style={{ color: 'red', fontWeight: 600 }}>
+        Sorry, answer was {correctAnswer}, try again!
+      </Text>
+      <View>
+        <Button
+          color='red'
+          title='CHECK ANSWER'
+          onPress={(e) => { setX(10); console.log(e) }}
+        />
+      </View>
+
+      {correctAnswer}
+      {userAnswer}
     </View>
   );
 }
